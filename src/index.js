3192bd22ff83b924814b67bced7e0eb279fcb5a2
Ron Maina <ronmwangi00@gmail.com>
2023-10-30 20:52:05 +0300

    Login/SignUppage

--- src/index.js
+++ src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '../src/components/App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom"
+
+// function getRandomColor() {
+//   const colors = ['#ff5733', '#00ff00', '#3366ff', '#ffff00', '#ff33cc'];
+//   const randomIndex = Math.floor(Math.random() * colors.length);
+//   return colors[randomIndex];
+// }
+
+// function ColoredApp() {
+//   const backgroundColor = getRandomColor();
+//   return <App backgroundColor={backgroundColor} />;
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <div className='bodt'>
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    
+      <App />
+ 
+    </BrowserRouter>
   </React.StrictMode>
+  </div>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
