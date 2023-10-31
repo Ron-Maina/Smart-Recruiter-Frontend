fc9f589407f20bf3923242851c1680563af5cdf4
Ron Maina <ronmwangi00@gmail.com>
2023-11-01 01:18:30 +0300

    Updated routes

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,14 @@ import { Route, Routes } from "react-router-dom"
 import SignUp from './SignUp';
 import SignIn from './SignIn';
 
-function App({ backgroundColor }) {
+function App() {
   return (
-    <div style={{ backgroundColor }}>
-    <Routes>
-      <Route exact path='/signup' element={<SignUp />} />
-      <Route exact path='/signin' element={<SignIn />} />
-    </Routes>
-    </div>
+    <>
+      <Routes>
+        <Route exact path='/signup' element={<SignUp />} />
+        <Route exact path='/signin' element={<SignIn />} />
+      </Routes>
+    </>
   );
 }
 
