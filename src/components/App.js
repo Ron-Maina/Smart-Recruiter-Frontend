5a96df40861c23a9415dfc41d942bc18880d1cea
Ron <134933484+Ron-Maina@users.noreply.github.com>
2023-11-02 10:09:26 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,27 +6,24 @@ import SignIn from './SignIn';
 import Mainpage from './Mainpage';
 import Sidebar from './Sidebar';
 import Landingpage from './Landingpage';
-import Interviewee from './Interviewee';
-import Intervieweesidebar from './Intervieweesidebar';
 
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Landingpage />} />
-        <Route exact path='/signup' element={<SignUp />} />
-        <Route exact path='/signin' element={<SignIn />} />
-        <Route exact path='/Interviewee' element={<Interviewee />} />
-        <Route exact path='/Intervieweesidebar' element={<Intervieweesidebar />} />
-        <Route exact path='/Sidebar' element={<Sidebar />} />
-         
-
-      </Routes>
-
-      <Routes>
-        <Route exact path='/homepage' element={<Mainpage />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path='/' element={<Landingpage />} />
+          <Route exact path='/signup' element={<SignUp />} />
+          <Route exact path='/signin' element={<SignIn />} />
+        </Routes>
+      </div>
+      <div>
+        {/* <Sidebar /> */}
+        <Routes>
+          <Route exact path='/homepage' element={<Mainpage />} />
+        </Routes>
+      </div>
     </>
   );
 }
