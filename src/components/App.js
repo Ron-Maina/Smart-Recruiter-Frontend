3f6944c336473f4e7a6f5aa064e271be67eb07c2
Peggy <peggykimotho@gmail.com>
2023-11-02 13:59:19 +0000

    Added interviewee list

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,16 @@ import SignIn from './SignIn';
 import Mainpage from './Mainpage';
 import Sidebar from './Sidebar';
 import Landingpage from './Landingpage';
+import Assessmentreviews from './Assessmentreviews';
+import Intervieweelist from './Intervieweelist';
 
 
 function App() {
+  const [assessment_id, setAssessment_id] = useState("")
+
+  function handleRender(id){
+    setAssessment_id(id)
+  }
   return (
     <>
       <div>
@@ -16,6 +23,9 @@ function App() {
           <Route path='/' element={<Landingpage />} />
           <Route exact path='/signup' element={<SignUp />} />
           <Route exact path='/signin' element={<SignIn />} />
+          <Route exact path='/Assessmentreviews' element={<Assessmentreviews onrender={handleRender}/>} />
+          <Route exact path='/Intervieweelist' element={<Intervieweelist assessment_id={assessment_id}/>} />
+
         </Routes>
       </div>
       <div>
